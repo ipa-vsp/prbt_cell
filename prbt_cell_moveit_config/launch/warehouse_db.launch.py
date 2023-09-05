9c04bbfc09c4cf781f2823d7caa99b1e427108a6
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2023-09-05 11:39:12 +0200

    remove ros2 components

--- prbt_cell_moveit_config/launch/warehouse_db.launch.py
+++
@@ -1,9 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_warehouse_db_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder(
-        "prbt_cell", package_name="prbt_cell_moveit_config"
-    ).to_moveit_configs()
-    return generate_warehouse_db_launch(moveit_config)
